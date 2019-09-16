f119c9f51b316fbb2d3555c45d9ef6cb5fd42044
nata100401 <ericanataliaparra@gmail.com>
2019-09-16 14:02:45 -0500

    chat

--- vistas/vistascliente/header.php
+++ vistas/vistascliente/header.php
@@ -125,7 +125,7 @@ include("carrito.php");
             </div> 
           </div>
           
-           <!-- <script>
+           <script>
              $('#exampleModal').on('show.bs.modal', event => {
                var button = $(event.relatedTarget);
                var modal = $(this);
@@ -142,5 +142,5 @@ include("carrito.php");
               });
             }
             function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
-          </script>    -->
+          </script>   
       
